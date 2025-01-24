46e4da4556bcd7de60a01f8fe6ca0dbf40f548d1
KoruJuriSan <mr.collejoulian@gmail.com>
2025-01-24 15:06:33 +0100

    style: updating home menu to be a little more "acceptable"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@ import idea from "@/types/idea"
 import Link from "next/link"
 
 const ideas: idea[] = [
-  {path: "/handwriting", title:"Handwriting animation", description: "Lorem Ipsum dolor sit amet", createdAt: new Date("2025-01-10")}
+  {path: "/handwriting", title:"Handwriting animation", description: "Handwirting text animation (svg)", createdAt: new Date("2025-01-10")},
 ]
 
 const ideasLi = ideas.map(({path, title, description, createdAt}: idea, key) => {
-  return <li key={key}>
+  return <li className=" p-4" key={key}>
     <Link className="underline" href={path}>
       <h3>{title}</h3>
     </Link>
       {description ? <p>{description}</p> : ""}
-      {createdAt ? <span>{createdAt.toDateString()}</span> : ""}
+      {createdAt ? <span className=" text-sm opacity-65 italic">{createdAt.toDateString()}</span> : ""}
   </li>
 })
 
@@ -22,8 +22,8 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main>
-      <h2>Some of my ideas</h2>
+    <main className=" flex flex-col items-center">
+      <h2 className=" text-2xl font-extrabold my-8 relative before:block before:absolute before:w-5/6 before:h-[2px] before:bg-current before:-bottom-1">Some of my ideas</h2>
       <ul>
         {ideasLi}
       </ul>
