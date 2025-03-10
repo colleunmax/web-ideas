1033943182a4f9b295a4ecbbef9ecf0baa9d486e
KoruJuriSan <mr.collejoulian@gmail.com>
2025-03-10 13:55:35 +0100

    fix: updating handwriting animatoin to be responsive

--- src/app/handwriting/page.tsx
+++ src/app/handwriting/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 export default function HandwritingPage() {
     return <>
         <main id="handwriting-main">
-            <div className="handwirting-wraper">
+            <div className="handwriting-wraper">
                 <h1>I am Colle</h1>
                 <div aria-hidden="true" className="handwriting-container">
                     <svg id="svg-1" width="22" height="37" viewBox="0 0 22 37" fill="none" xmlns="http://www.w3.org/2000/svg">
